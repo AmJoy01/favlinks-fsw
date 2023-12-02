ef24fda6bd4af68f4b5cabcba2324fba2f89e4a6
AmJoy01 <amber.lewis01@gmail.com>
2023-12-01 21:13:28 -0500

    build server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+const PORT = 3000
+
+const clientPath = path.resolve(__dirname, "../client/dist")
+
+app.use(express.static(clientPath))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+})
+
+app.listen(PORT, () => {
+
+    console.log(`Server listening on port ${PORT}`)
+})
