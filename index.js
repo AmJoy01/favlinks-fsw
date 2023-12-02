f7b4966772277138bcf4c20318e1880adc58e89c
AmJoy01 <amber.lewis01@gmail.com>
2023-12-01 21:26:42 -0500

    fixed localhost:3000 error

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const app = express()
 
 const PORT = 3000
 
-const clientPath = path.resolve(__dirname, "../client/dist")
+const clientPath = path.resolve(__dirname, "../favlinks-fsw/client/dist")
 
 app.use(express.static(clientPath))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../favlinks-fsw/client/dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
-
     console.log(`Server listening on port ${PORT}`)
 })
